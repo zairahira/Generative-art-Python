8b9cebe42b998500d8f0edae9f2d48d531404d08
Zaira <33151350+zairahira@users.noreply.github.com>
2022-06-24 01:32:18 +0500

    comments added

--- Eulers theorem/eulers-theme.py
+++ Eulers theorem/eulers-theme.py
@@ -14,16 +14,18 @@ set_theme(tracer_value=2,hide_turtle=False)
 euler_curve(step_size=5,  angle_step=0.66, n_steps=10000)
 
 # tracer _value = how fast tracer moves
+
 """set_theme(tracer_value=1, hide_turtle=False)
 euler_curve(step_size=40,  angle_step=1.00, n_steps=600)
 """
 
-
+# try these arguments and see the curves.
 """
 set_theme(tracer_value=10)
 euler_curve(step_size=7,  angle_step=0.66, n_steps=10000)
 """
 
+# try these arguments and see the curves.
 """"
 set_theme(tracer_value=10, hide_turtle=False)
 euler_curve(step_size=3,  angle_step=1.99, n_steps=100000)
